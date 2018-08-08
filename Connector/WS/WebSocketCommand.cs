26bf8e91e2b4f86104cb67c0e21ee4927cbe0570
Usachev Denis <usachyovd@terralink.ru>
2018-08-08 08:29:32 +0300

    Web sockets added

---
+++ Connector/WS/WebSocketCommand.cs
@@ -0,0 +1,9 @@
+﻿namespace Connector.WS
+{
+    public class WebSocketCommand
+    {
+        public string op { get; set; }
+        public string[] args { get; set; }
+        public bool success { get; set; }
+    }
+}
---
+++ Connector/WS/WebSocketFeed.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketSharp;
+
+namespace Connector.WS
+{
+    public class WebSocketFeed
+    {
+        private WebSocket _webSocket;
+
+        public string ConnectionString { get; set; }
+
+
+        public WebSocketFeed(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
+        public bool Connect()
+        {
+            _webSocket = new WebSocket(ConnectionString);
+            _webSocket.OnMessage += MessageReceived;
+            _webSocket.Connect();
+            return true;
+        }
+
+        private void MessageReceived(object sender, MessageEventArgs e)
+        {
+            Console.WriteLine(e.Data);
+        }
+
+        public bool Subscribe(List<string> channels, string instrument)
+        {
+            var message = new WebSocketCommand()
+            {
+                op = "subscribe",
+                args = (new List<string> { "orderBookL2", "instrument" }).ToArray()
+            };
+            return true;
+        }
+    }
+}
